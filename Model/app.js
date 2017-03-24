a05385eb059ae61e7b3db02db9a1bdae8b2815dc
andela-ofaith <faith.omokaro@andela.com>
2017-03-24 12:49:48 +0100

    add all files

---
+++ Model/app.js
@@ -0,0 +1,90 @@
+const app = angular.module('inverted_index', []);
+app.controller('myController', ['$scope', ($scope) => {
+  const invertedIndex = new Index();
+  $scope.documents = [];
+  $scope.fileNames = [];
+  $scope.numberOfDocuments = {};
+  $scope.indices = {};
+  $scope.showUploaded = false;
+  $scope.showTable = false;
+  $scope.showSearch = false;
+  $scope.file = {};
+  $scope.indexAtr = {};
+  $scope.filesToSearch = [];
+  $scope.fileContent = {};
+  $scope.search = {};
+
+  const file = document.getElementById('files');
+  const indexBtn = document.getElementById('index');
+  const selectFiles = document.getElementById('selectFiles');
+  const search = document.getElementById('searchTerms');
+  const fileName = $scope.selectFiles;
+
+  file.addEventListener('change', () => {
+    $scope.uploadFile(file);
+  });
+
+  $scope.uploadFile = (file) => {
+    file = file.files;
+    for (let i = 0; i < file.length; i += 1) {
+      const fileExtension = file[i].name.split('.').pop();
+      if (fileExtension !== 'json') {
+        return toastr.error('This is not a json file');
+      }
+      if ($scope.fileNames.includes(file[i].name)) {
+        return toastr.error(`${file[i].name} has already been uploaded`, 'Error');
+      }
+      $scope.documents = $scope.fileNames.push(file[i].name);
+      $scope.showUploaded = true;
+      $scope.$apply($scope.documents);
+      toastr.success(`${file[i].name} uploaded successfully`, 'Success');
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        $scope.fileContent[file[i].name] = reader.result;
+        $scope.$apply();
+      };
+      reader.readAsText(file[i]);
+    }
+  };
+
+  $scope.createIndex = () => {
+    const fileName = document.getElementById('selectFile').value;
+    let fileContent = $scope.fileContent[fileName];
+
+
+    try {
+      invertedIndex.createIndex(fileName, fileContent);
+    } catch (err) {
+      toastr.error(err.message);
+    }
+
+    const index = invertedIndex.getIndex(fileName);
+    $scope.indices[fileName] = index;
+
+    fileContent = JSON.parse(fileContent);
+    $scope.fileContent[fileName] = fileContent;
+    const length = fileContent.length;
+    const temp = [];
+    for (let i = 0; i < length; i++) {
+      temp.push(i);
+    }
+
+    $scope.numberOfDocuments[fileName] = temp;
+    $scope.filesToSearch.push(fileName);
+    $scope.showTable = true;
+    console.log($scope.indices);
+  };
+
+
+  $scope.searchIndex = () => {
+    const filename = document.getElementById('selectSearchFile').value;
+    const words = document.getElementById('searchBox').value;
+    console.log(filename);
+
+    $scope.searchFileName = filename;
+    $scope.searchResult = invertedIndex.searchIndex(words, filename);
+    $scope.showSearch = true;
+  };
+}]);
+
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const MinifyCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const eslint = require('gulp-eslint');
+const browserSync = require('browser-sync');
+
+gulp.task('browserSync', () => {
+  browserSync({
+    server: {
+      baseDir: './',
+      index: 'public/index.html',
+    },
+    port: 3000,
+    files: ['Model/**', 'public/**/*.*'],
+    open: false,
+  });
+});
+
+
+gulp.task('default', ['MinifyCSS', 'browserSync'], () => {});
+
+gulp.task('MinifyCSS', () => {
+  return gulp.src('style.css')
+  .pipe(MinifyCSS())
+  .pipe(gulp.dest('style.min.css'));
+});
+
+gulp.task('say', () => {
+  console.log('The task is running....');
+});
+
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+  return gulp.src(['src/*.js', '!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
